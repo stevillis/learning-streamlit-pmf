34b8aae6f39b1e7b4c0670c9511ea0473ef0e715
Stévillis Sousa <stevillis@hotmail.com>
2023-09-30 01:45:13 -0400

    feat: media functions

---
+++ 3_funcoes_de_media.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.header("Funções de mídia")
+st.subheader("st.image()")
+st.image("chris-charles-hummingbird-unsplash.jpg", width=450, caption="Hummingbird")
+
+st.subheader("st.audio()")
+st.audio("morf_na_live.m4a")
+
+st.subheader("st.video()")
+st.video("https://www.youtube.com/watch?v=YZfDuuEP9zE")
